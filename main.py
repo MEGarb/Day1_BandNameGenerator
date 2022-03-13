5650688d80e453692eea39309396807812e98f34
MEGarb <mik.garber@gmail.com>
2022-03-12 18:44:22 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+city = input("What city did you grow up in?\n")
+pet = input("What was the name of your favorite pet?\n")
+print("The name of your band is:  " + city + ' ' + pet)
